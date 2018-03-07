6acef9c67a7f83cfaf8a4a54d854af7ff99a9a3f
Tim Stephansen <tim@landmarkdevs.com>
2018-03-07 15:28:43 -0500

    Initial commit.

---
+++ WixBuilder/MainWindowViewModel.cs
@@ -0,0 +1,152 @@
+﻿using LandmarkDevs.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WixBuilder
+{
+    public class MainWindowViewModel : Observable
+    {
+        public MainWindowViewModel()
+        {
+            FileDictionary = new Dictionary<string, Dictionary<string, string>>();
+            DirectoryDictionary = new Dictionary<string, DirectoryInfo>();
+            Directories = new ObservableDictionary<string, string>();
+            AddDirectoryCommand = new RelayCommand(AddDirectory);
+            RemoveDirectoryCommand = new RelayCommand<string>(RemoveDirectory);
+            ProcessCommand = new RelayCommand(async () => await ProcessDependencies());
+        }
+
+        #region Commands
+        public RelayCommand AddDirectoryCommand { get; }
+        public RelayCommand<string> RemoveDirectoryCommand { get; }
+        public RelayCommand ProcessCommand { get; }
+        #endregion
+
+        #region Methods
+        public void AddDirectory()
+        {
+            if (string.IsNullOrEmpty(DirectoryName) || string.IsNullOrEmpty(VariableName)) return;
+            Directories.Add(DirectoryName, VariableName);
+            DirectoryName = string.Empty;
+            VariableName = string.Empty;
+        }
+
+        public void RemoveDirectory(string directoryName) => Directories.Remove(directoryName);
+
+        public Task ProcessDependencies()
+        {
+            return Task.Run(() =>
+            {
+                GetDirectoryInformation();
+                foreach (var info in DirectoryDictionary.Values)
+                {
+                    GetListOfFilesInDirectory(info);
+                }
+                WriteDependencyInformationToFile();
+                Application.Current.Dispatcher.Invoke(() => { MessageBox.Show("Complete!"); });
+            });
+        }
+
+        public void GetListOfFilesInDirectory(DirectoryInfo directoryInfo)
+        {
+            GetFilesInDirectory(directoryInfo.GetFiles("*", SearchOption.TopDirectoryOnly));
+        }
+
+        public void GetFilesInDirectory(FileInfo[] files)
+        {
+            foreach (var file in files)
+            {
+                if (FileDictionary.ContainsKey(file.Name))
+                    continue;
+                FileDictionary.Add(file.Name, new Dictionary<string, string> { { file.Directory.FullName, file.Name } });
+            }
+        }
+
+        public void GetDirectoryInformation()
+        {
+            foreach (var directoryPath in Directories.Keys)
+            {
+                var dir = new DirectoryInfo(directoryPath);
+                DirectoryDictionary.Add(dir.FullName, dir);
+            }
+        }
+
+        public void WriteDependencyInformationToFile()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<Fragment>");
+            sb.AppendLine("<DirectoryRef Id=\"InstallDirectory\">");
+            var counter = 0;
+            foreach (var file in FileDictionary)
+            {
+                var value = file.Value;
+                var variableName = Directories.First(c => c.Key == value.Keys.ElementAt(0) || c.Key.Contains(value.Keys.ElementAt(0))).Value;
+                sb.AppendLine($"<Component Id=\"cmp{counter}\" Guid=\"{Guid.NewGuid()}\">");
+                sb.AppendLine(
+                    $"<File Id=\"file{counter}\" KeyPath=\"yes\" Source=\"{variableName}{value.Values.ElementAt(0)}\" />");
+                sb.AppendLine("</Component>");
+                counter++;
+            }
+            sb.AppendLine("</DirectoryRef>");
+            sb.AppendLine("</Fragment>");
+            if(File.Exists(DependencyFilePath)) File.Delete(DependencyFilePath);
+            using (var fs = new FileStream(DependencyFilePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                var bytes = new UTF8Encoding(true).GetBytes(sb.ToString());
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Close();
+            }
+        }
+        #endregion
+
+        #region Fields and Properties
+        public Dictionary<string, Dictionary<string, string>> FileDictionary
+        {
+            get => _fileDictionary;
+            set => Set(ref _fileDictionary, value);
+        }
+        private Dictionary<string, Dictionary<string, string>> _fileDictionary;
+
+        public Dictionary<string, DirectoryInfo> DirectoryDictionary
+        {
+            get => _directoryDictionary;
+            set => Set(ref _directoryDictionary, value);
+        }
+        private Dictionary<string, DirectoryInfo> _directoryDictionary;
+        
+        public string DependencyFilePath
+        {
+            get => _dependencyFilePath;
+            set => Set(ref _dependencyFilePath, value);
+        }
+        private string _dependencyFilePath;
+
+        public ObservableDictionary<string, string> Directories
+        {
+            get => _directories;
+            set => Set(ref _directories, value);
+        }
+        private ObservableDictionary<string, string> _directories;
+
+        public string DirectoryName
+        {
+            get => _directoryName;
+            set => Set(ref _directoryName, value);
+        }
+        private string _directoryName;
+
+        public string VariableName
+        {
+            get => _variableName;
+            set => Set(ref _variableName, value);
+        }
+        private string _variableName;
+        #endregion
+    }
+}
+
---
+++ WixBuilder/ObservableDictionary.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+// ReSharper disable InheritdocConsiderUsage
+
+namespace WixBuilder
+{
+    /// <summary>
+    /// Provides a dictionary for use with data binding. This was originally in
+    /// the parallel programming sample code (https://code.msdn.microsoft.com/ParExtSamples)
+    /// and was modified to be used as a regular dictionary with change notification.
+    /// </summary>
+    /// <typeparam name="TKey">Specifies the type of the keys in this collection.</typeparam>
+    /// <typeparam name="TValue">Specifies the type of the values in this collection.</typeparam>
+    [DebuggerDisplay("Count={Count}")]
+    public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private readonly Dictionary<TKey, TValue> _dictionary;
+
+        public ObservableDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        /// <summary>
+        /// Event raised when the collection changes.
+        /// </summary>
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        /// <summary>
+        /// Event raised when a property on the collection changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies observers of CollectionChanged or PropertyChanged of an update to the dictionary.
+        /// </summary>
+        private void NotifyObserversOfChange()
+        {
+            var collectionHandler = CollectionChanged;
+            var propertyHandler = PropertyChanged;
+            if (collectionHandler == null && propertyHandler == null) return;
+            collectionHandler?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            if (propertyHandler == null) return;
+            propertyHandler(this, new PropertyChangedEventArgs("Count"));
+            propertyHandler(this, new PropertyChangedEventArgs("Keys"));
+            propertyHandler(this, new PropertyChangedEventArgs("Values"));
+        }
+
+        /// <summary>
+        /// Attempts to add an item to the dictionary, notifying observers of any changes.
+        /// </summary>
+        /// <param name="item">The item to be added.</param>
+        /// <returns>Whether the add was successful.</returns>
+        private void TryAddWithNotification(KeyValuePair<TKey, TValue> item) => TryAddWithNotification(item.Key, item.Value);
+
+        /// <summary>
+        /// Attempts to add an item to the dictionary, notifying observers of any changes.
+        /// </summary>
+        /// <param name="key">The key of the item to be added.</param>
+        /// <param name="value">The value of the item to be added.</param>
+        /// <returns>Whether the add was successful.</returns>
+        private void TryAddWithNotification(TKey key, TValue value)
+        {
+            _dictionary.Add(key, value);
+            NotifyObserversOfChange();
+        }
+
+        /// <summary>
+        /// Attempts to remove an item from the dictionary, notifying observers of any changes.
+        /// </summary>
+        /// <param name="key">The key of the item to be removed.</param>
+        /// <returns>Whether the removal was successful.</returns>
+        private bool TryRemoveWithNotification(TKey key)
+        {
+            var result = _dictionary.Remove(key);
+            if (result) NotifyObserversOfChange();
+            return result;
+        }
+
+        /// <summary>
+        /// Attempts to add or update an item in the dictionary, notifying observers of any changes.
+        /// </summary>
+        /// <param name="key">The key of the item to be updated.</param>
+        /// <param name="value">The new value to set for the item.</param>
+        /// <returns>Whether the update was successful.</returns>
+        private void UpdateWithNotification(TKey key, TValue value)
+        {
+            _dictionary[key] = value;
+            NotifyObserversOfChange();
+        }
+
+        #region ICollection<KeyValuePair<TKey,TValue>> Members
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item) => TryAddWithNotification(item);
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Clear()
+        {
+            ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Clear();
+            NotifyObserversOfChange();
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item) => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Contains(item);
+
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).CopyTo(array, arrayIndex);
+        }
+
+        int ICollection<KeyValuePair<TKey, TValue>>.Count => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Count;
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).IsReadOnly;
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item) => TryRemoveWithNotification(item.Key);
+
+        #endregion
+
+        #region IEnumerable<KeyValuePair<TKey,TValue>> Members
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+
+        public IEnumerator GetEnumerator() => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+
+        #endregion
+
+        #region IDictionary<TKey,TValue> Members
+        public void Add(TKey key, TValue value) => TryAddWithNotification(key, value);
+
+        public bool ContainsKey(TKey key) => _dictionary.ContainsKey(key);
+
+        public ICollection<TKey> Keys => _dictionary.Keys;
+
+        public bool Remove(TKey key) => TryRemoveWithNotification(key);
+
+        public bool TryGetValue(TKey key, out TValue value) => _dictionary.TryGetValue(key, out value);
+
+        public ICollection<TValue> Values => _dictionary.Values;
+
+        public TValue this[TKey key]
+        {
+            get => _dictionary[key];
+            set => UpdateWithNotification(key, value);
+        }
+        #endregion
+
+        public int Count => _dictionary.Count;
+    }
+}
